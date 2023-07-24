07a3c85d114a777f7ea05f67d32829a55b6872ea
Peter Tumiel <petertumiel@gmail.com>
2023-07-24 22:20:13 +0200

    Get cpu temp

--- temperature.c
+++ temperature.c
@@ -1,18 +1,51 @@
 #include <stdio.h>
+#include <errno.h>
+#include <glob.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
-    FILE *temperatureFile;
-    double T;
-    temperatureFile = fopen ("/sys/class/thermal/thermal_zone0/temp", "r");
-    if (temperatureFile == NULL)
-    {
-        printf("Error: Unable to open thermal_zone0/temp\n");
-        return(-1);
+char* concat(const char *s1, const char *s2);
+
+int main(void) {
+    glob_t paths;
+    FILE *thermal;
+    int temperature;
+    glob("/sys/class/thermal/thermal_zone*",GLOB_TILDE,NULL,&paths);
+    for(unsigned int i=0;i<paths.gl_pathc;i++){
+    	thermal=fopen(concat(paths.gl_pathv[i],"/temp"),"r");
+        if(thermal==NULL){
+		perror("Failed");
+		return 1;
+	}
+	if(fscanf(thermal,"%d",&temperature) !=1){
+		perror("Failed to get temp");
+		fclose(thermal);
+	}
+    	printf("========\n");
+	printf("|      |\n");
+
+    	printf("| temp |\n");
+	printf("|  %i  |\n",temperature/1000);
+    	printf("|      |\n");
+	printf("|      |\n");
+    	printf("========\n");
     }
-    fscanf (temperatureFile, "%lf", &T);
-    T /= 1000;
-    printf ("The CPU temperature is: %.2lf C\n", T);
-    fclose (temperatureFile);
-    return 0;
+    fclose(thermal);
 }
 
+char* concat(const char *s1, const char *s2) {
+    size_t len = strlen(s1) + strlen(s2) + 1;
+
+    char *result = malloc(len * sizeof(char));
+
+    if (result == NULL) {
+        printf("Failed to allocate memory\n");
+        exit(1); 
+    }
+
+    strcpy(result, s1);
+
+    strcat(result, s2);
+
+    return result;
+}
