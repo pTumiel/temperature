afe884487bb1140cd54712a17735da03b712ee10
Peter Tumiel <petertumiel@gmail.com>
2023-07-25 10:04:14 +0200

    Listing all the thermal zones

--- temperature.c
+++ temperature.c
@@ -5,11 +5,37 @@
 #include <stdlib.h>
 
 char* concat(const char *s1, const char *s2);
-
+void layout(int columns,int temp[]);
 int main(void) {
     glob_t paths;
     FILE *thermal;
     int temperature;
+
+    FILE* fp;
+    char buffer[128];
+    
+    // Open the file for reading
+    fp = fopen("/proc/cpuinfo", "r");
+    if (fp == NULL) {
+        printf("Could not open /proc/cpuinfo\n");
+        return 1;
+    }
+
+    // Read the file line by line
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        // If the line starts with "model name", print it
+        if (strncmp(buffer, "model name", 10) == 0) {
+            printf("%s", buffer);
+            break;
+        }
+    }
+
+    // Close the file
+    fclose(fp);
+
+
+
+
     glob("/sys/class/thermal/thermal_zone*",GLOB_TILDE,NULL,&paths);
     for(unsigned int i=0;i<paths.gl_pathc;i++){
     	thermal=fopen(concat(paths.gl_pathv[i],"/temp"),"r");
@@ -21,14 +47,6 @@ int main(void) {
 		perror("Failed to get temp");
 		fclose(thermal);
 	}
-    	printf("========\n");
-	printf("|      |\n");
-
-    	printf("| temp |\n");
-	printf("|  %i  |\n",temperature/1000);
-    	printf("|      |\n");
-	printf("|      |\n");
-    	printf("========\n");
     }
     fclose(thermal);
 }
@@ -49,3 +67,30 @@ char* concat(const char *s1, const char *s2) {
 
     return result;
 }
+
+void layout(int columns,int temps[columns]){
+	for(int i=0;i<columns;i++){
+		printf("========");
+	}
+	printf("=\n");
+	for(int i=0;i<2;i++){
+		for(int b=0;b<columns;b++){
+			printf("|       ");
+		}
+		printf("|\n");
+
+	}
+	for(int b=0;b<columns;b++){
+                printf("| %i   ",temps[columns]/1000      );
+        }
+        printf("|\n");
+
+
+	for(int i=0;i<columns;i++){
+        	printf("========");
+        }
+	printf("=\n");
+
+}
+
+
