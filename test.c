cafef7adc6b737628fb7c8e9413630eadc32e545
Peter Tumiel <petertumiel@gmail.com>
2023-07-25 09:45:41 +0200

    Layout of all thermal zones listed

---
+++ test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+int colums=5;	
+for(int i=0;i<colums;i++){
+     printf("========");
+}
+printf("=\n");
+for(int i=0;i<colums;i++){
+     printf("|       ");
+}
+printf("|\n");
+
+for(int i=0;i<colums;i++){
+     printf("|       ");
+}
+printf("|\n");
+
+for(int i=0;i<colums;i++){
+     printf("|       ");
+
+}
+printf("|\n");
+
+for(int i=0;i<colums;i++){
+     printf("========");
+}
+printf("=\n");
+
+
+
+
+}
