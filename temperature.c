d66e5f887b056c9dbbaacddafba58aee249cb2ed
Peter Tumiel <petertumiel@gmail.com>
2023-07-26 18:28:44 +0200

    Gpu temp

--- temperature.c
+++ temperature.c
@@ -3,15 +3,38 @@
 #include <glob.h>
 #include <string.h>
 #include <stdlib.h>
-
+#define MAX_GPU_NAME 128
 char* concat(const char *s1, const char *s2);
-void layout(int columns,int temps[columns]);
+void layout(int columns,int temps[columns],int b);
 
 int main(void) {
     glob_t paths;
     FILE *thermal;
     int temperature;
+    FILE *gp;
+    char output[1024];
+    char gpu_name[MAX_GPU_NAME];
+    int gpu_temp;
 
+    /* Open the command for reading. */
+    gp = popen("/usr/bin/nvidia-smi --query-gpu=name,temperature.gpu --format=csv,noheader", "r");
+    if (gp == NULL) {
+        printf("Failed to run command\n" );
+        exit(1);
+    }
+
+    /* Read the output a line at a time. */
+    while (fgets(output, sizeof(output)-1, gp) != NULL) {
+        /* Parse the output. */
+        sscanf(output, "%[^,], %d", gpu_name, &gpu_temp);
+    
+    }
+
+    /* close */
+    int gpuarray[]={gpu_temp};
+    pclose(gp);
+    layout(1,gpuarray,1);
+    printf("%s %i",gpu_name,gpu_temp);
 
     FILE* fp;
     char buffer[128];
@@ -32,7 +55,7 @@ int main(void) {
     // Close the file
     fclose(fp);
 
-
+    
     //read temp of cpu
     glob("/sys/class/thermal/thermal_zone*",GLOB_TILDE,NULL,&paths);
     int temps[paths.gl_pathc];
@@ -51,7 +74,7 @@ int main(void) {
     }
     fclose(thermal);
     
-    layout(paths.gl_pathc,temps);
+    layout(paths.gl_pathc,temps,0);
 }
 
 char* concat(const char *s1, const char *s2) {
@@ -71,20 +94,33 @@ char* concat(const char *s1, const char *s2) {
     return result;
 }
 
-void layout(int columns,int temps[columns]){
+void layout(int columns,int temps[columns],int flag){
+    
 	for(int i=0;i<columns;i++){
 		printf("============");
 	}
 	printf("=\n");
 
     for(int b=0;b<columns;b++){
-        printf("|temp zone %i",b);
+        if(flag==0){
+            printf("|temp zone %i",b);
+        }
+        else{
+            printf("|gpu ");
+            break;
+        }
     }
     printf("|\n");
 
 	
 	for(int b=0;b<columns;b++){
+        if(flag==0){
                 printf("|    %iC    ",temps[b]/1000);
+            }
+        }
+        else{
+            printf("|    %iC    ",temps[b])
+
         }
         printf("|\n");
 
